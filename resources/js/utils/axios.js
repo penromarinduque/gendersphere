f512c3fda871dd8d08b1a85146376fba5181a176
penromarinduque <penromarinduquedenr@gmail.com>
2024-11-12 15:07:21 +0800

    fifth commit

--- resources/js/utils/axios.js
+++ resources/js/utils/axios.js
@@ -1,8 +1,8 @@
 import axiosLib from "axios";
 
 const axios = axiosLib.create({
-    // baseURL: "https://gendersphere.vercel.app/api",
-    baseURL: "http://192.168.60.11:9001",
+    baseURL: "https://gendersphere.vercel.app/api",
+    // baseURL: "http://192.168.60.11:9001",
     headers: {
         Accept: "application/json",
     }
