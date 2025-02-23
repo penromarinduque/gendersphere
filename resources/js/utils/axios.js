0e50201521cfe85e672e117c2169172c0f820bb0
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2025-02-23 23:44:43 +0800

    change the api url

--- resources/js/utils/axios.js
+++ resources/js/utils/axios.js
@@ -5,7 +5,7 @@ const axios = axiosLib.create({
     // baseURL: "http://192.168.60.11:9001",
     // baseURL: "https://gendersphere.vercel.app/api",
     // baseURL: "https://gendersphere.penromarinduque.gov.ph",
-    baseURL: import.meta.env.API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         Accept: "application/json",
     }
