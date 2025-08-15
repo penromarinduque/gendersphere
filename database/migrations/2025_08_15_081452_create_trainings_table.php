3cc34b4cf384f5bfb51967e19093afb132aee6eb
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2025-08-15 08:25:07 +0800

    Compare old to new commits and restore the changes

---
+++ database/migrations/2025_08_15_081452_create_trainings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trainings', function (Blueprint $table) {
+            $table->id();
+            $table->string('training_title');
+            $table->dateTime('training_start');
+            $table->dateTime('training_end');
+            $table->integer('duration_hours');
+            $table->string('learning_description_type');
+            $table->string('sponsor_facilitator');
+            $table->foreignId('office_id')->nullable()->constrained()->onDelete('set null');
+            $table->timestamps(); // created_at and updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trainings');
+    }
+};
